988ebfcaf8b5f09c892367a875f9e63db15da5c1
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-03-05 15:50:54 +0400

    Created Car entity and created link one-to-one between Car and User classes

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -19,6 +19,10 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne
+   @JoinColumn(name="car_id")
+   private Car car;
+
    public User() {}
    
    public User(String firstName, String lastName, String email) {
